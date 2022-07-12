991c9645dcff6ec5b9f92fcbd55f2c8b216ba5aa
Cengiz TORU <cengiztoru@gmail.com>
2022-07-13 01:45:27 +0300

    Prepared Like Button for PostItem

---
+++ app/src/main/java/com/cengiztoru/linkedindashboard/ui/components/LikeButton.kt
@@ -0,0 +1,31 @@
+package com.cengiztoru.linkedindashboard.ui.components
+
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Icon
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.painterResource
+import com.cengiztoru.linkedindashboard.R
+
+@Composable
+fun LikeButton() {
+    var reactionsShown by remember { mutableStateOf(false) }
+
+    Icon(
+        painterResource(R.drawable.ic_like), "LikeButton", modifier = Modifier
+            .wrapContentSize()
+            .pointerInput(Unit) {
+                detectTapGestures(
+                    onLongPress = {
+                        reactionsShown = reactionsShown.not()
+                    },
+                )
+            }
+    )
+
+    ReactionDialog(reactionsShown) {
+        reactionsShown = reactionsShown.not()
+    }
+}
