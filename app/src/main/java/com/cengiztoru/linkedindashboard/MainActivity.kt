f3261963754eca4264fae4e0326e0ac6ee94ed0d
Cengiz TORU <cengiztoru@gmail.com>
2022-07-13 01:45:37 +0300

    Prepared Post Item

--- app/src/main/java/com/cengiztoru/linkedindashboard/MainActivity.kt
+++ app/src/main/java/com/cengiztoru/linkedindashboard/MainActivity.kt
@@ -6,10 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.cengiztoru.linkedindashboard.ui.components.PostItem
 import com.cengiztoru.linkedindashboard.ui.theme.LinkedInDashboardTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +22,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    PostItem()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     LinkedInDashboardTheme {
-        Greeting("Android")
+        PostItem()
     }
 }
---
+++ app/src/main/java/com/cengiztoru/linkedindashboard/ui/components/PostItem.kt
@@ -0,0 +1,8 @@
+package com.cengiztoru.linkedindashboard.ui.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PostItem() {
+    LikeButton()
+}
